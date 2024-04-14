6ee0c210e223b3f2fd57c93b6796cd76b1380418
lldscc <858400061@qq.com>
2024-04-14 12:02:25 +0800

    ✨ feat(注册接口): 数据是否合法校验（不为空）

--- router_handler/user.js
+++ router_handler/user.js
@@ -3,6 +3,16 @@
  */
 // 注册用户的处理函数
 exports.regUser = (req, res) => {
+
+    // 接收post请求的数据
+    const userinfo = req.body
+    // 可接受body体的内容
+    // console.log(userinfo);
+    // 判断是否合法
+    if(!userinfo.username || !userinfo.password){
+      // 不合法，返回客户端错误信息
+      return res.send({status:1,messsage:'用户名或密码不为空！！'})
+    }
     res.send('reguser OK')
   }
   
