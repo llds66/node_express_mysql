4f21a663de274b1f69ac537c1a2d4ba7bbba2a0e
lldscc <858400061@qq.com>
2024-04-14 11:14:34 +0800

    🎉 init: 初始化node项目

---
+++ app.js
@@ -0,0 +1,16 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+// 导入 cors 中间件
+const cors = require('cors')
+// 将 cors 注册为全局中间件
+app.use(cors())
+// 解析 application/x-www-form-urlencoded格式的表单数据的中间件
+app.use(express.urlencoded({ extended: false }))
+// write your code here...
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+  console.log('api server running at http://127.0.0.1:3007')
+})
