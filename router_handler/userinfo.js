095afdd29d5900da8656fe0581cb4859b4e9bf8f
lldscc <858400061@qq.com>
2024-04-16 09:01:46 +0800

    ✨ feat(个人中心): 连接数据库，获取用户信息

--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -3,6 +3,18 @@
  */
 
 // 获取用户信息
+const { date } = require('joi')
+const db = require('../db/index')
+
 exports.getUserInfo = (req, res) => {
-    res.send('用户信息')
+    const sql = `select id,username,nickname,email,user_pic from ev_users where id=?` // sql语句
+    db.query(sql, req.user.id, (err, results) => {
+        if (err) return res.cc(err) // sql执行失败
+        if (results.length !== 1) return res.cc('获取用户信息失败') //影响行数错误
+        res.send({
+            status: 0,
+            message: '获取用户信息成功',
+            data: results[0]
+        })
+    })
 }
