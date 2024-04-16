a1af8817ff621fac157e97dd2230467f5d4510fe
lldscc <858400061@qq.com>
2024-04-16 09:20:01 +0800

    ✨ feat(个人中心-更新信息): 实现更新检验

--- router/userinfo.js
+++ router/userinfo.js
@@ -4,11 +4,12 @@
 
 const express = require('express')
 const router = express.Router()
-const userinfo_handler = require('../router_handler/userinfo')
-
+const userinfo_handler = require('../router_handler/userinfo')  //封装路由处理函数
+const expressJoi = require('@escook/express-joi') //数据检验
+const { update_userinfo_schema } = require('../schema/user')
 // 1.获取用户信息模块
 router.get('/userinfo', userinfo_handler.getUserInfo)
 
 // 2.更新用户信息模块
-router.post('/userinfo', userinfo_handler.updateUserInfo)
+router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
 module.exports = router
