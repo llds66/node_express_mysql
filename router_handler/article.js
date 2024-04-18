01622dab01d2ec561148a5157539128fc90689fc
lldscc <858400061@qq.com>
2024-04-18 09:26:30 +0800

    ✨ feat(文章分类管理): 完成获取文章分类列表

--- router_handler/article.js
+++ router_handler/article.js
@@ -1,6 +1,18 @@
 /**
  * 获取文章列表分类的模块
  */
-exports.getArticleCates = (req,res) =>{
-    res.send('获取列表成功。。。')
+const db = require('../db/index')
+
+exports.getArticleCates = (req, res) => {
+    const sql = 'select * from ev_article_cate where is_delete=0 order by id asc'
+    db.query(sql, (err, results) => {
+        if (err) return res.cc(err)
+        res.send({
+            status: 0,
+            message: '获取文章分类列表成功...',
+            data: results
+
+        })
+    })
+
 }
