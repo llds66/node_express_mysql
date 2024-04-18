69513b0e3261f6b80ff272a3713fc6fefde98288
lldscc <858400061@qq.com>
2024-04-18 16:48:12 +0800

    ✨ feat(文章分类管理-id更新文章分类): 实现通过Id更新文章分类

--- router_handler/article.js
+++ router_handler/article.js
@@ -1,5 +1,5 @@
 /**
- * 文章列表分类
+ * 文章列表分类 请求逻辑处理模块
  */
 const db = require('../db/index')
 
@@ -63,6 +63,18 @@ exports.getArticleById = (req,res) =>{
 
 // 5.根据id更新文章分类
 exports.updateCateById = (req,res) =>{
-    res.send('根据id更新成功！')
+    const sql = `select * from ev_article_cate where Id<>? and (name=? or alias=?)`
+    db.query(sql,[req.body.Id,req.body.name,req.body.alias],(err,results) =>{
+        if (err) return res.cc(err)
+        if (results.length === 2) return res.cc('名称与别名已占用,请更换重试。')
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('名称已占用,请更换重试。')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('别名已占用,请更换重试。')
+        const sql = `update ev_article_cate set ? where Id=?`
+        db.query(sql,[req.body,req.body.Id],(err,results) =>{
+            if (err) return res.cc(err)
+            if (results.affectedRows !== 1) return res.cc('更新文章分类失败！')
+            res.cc('更新文章分类成功！',0)
+        })
+    })
 }
 
