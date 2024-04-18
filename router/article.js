5a18b7601dab8c959211527a7b393499271dad96
lldscc <858400061@qq.com>
2024-04-18 17:27:05 +0800

    ✨ feat(文章发布): 验证数据

--- router/article.js
+++ router/article.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const article_handler = require('../router_handler/article')
+const article_handler = require('../router_handler/article') //请求处理逻辑
 
-router.post('/add',upload.single('cover_img'), article_handler.addArticle)
+const expressJoi = require('@escook/express-joi')
+const { add_article_schema } = require('../schema/article') //验证规则对象
+
+router.post('/add',upload.single('cover_img'),expressJoi(add_article_schema),  article_handler.addArticle)
 
 module.exports = router
--- router_handler/article.js
+++ router_handler/article.js
@@ -1,15 +1,17 @@
 /**
  * 文章发布请求函数逻辑
  */
+
 const multer = require('multer')// 导入解析 formdata 格式表单数据的包
 const path = require('path')// 导入处理路径的核心模块
 const upload = multer({ dest: path.join(__dirname, '../uploads') })// 创建 multer 的实例对象，通过 dest 属性指定文件的存放路径
 
 // 1.发布新文章的处理函数
 exports.addArticle = (req, res) => {
-    console.log(req.body) // 文本类型的数据
-    console.log('--------分割线----------')
-    console.log(req.file) // 文件类型的数据
-
+    // console.log(req.body) 
+    // console.log('--------分割线----------')
+    // console.log(req.file) 
+        // 手动判断是否上传了文章封面
+  if (!req.file || req.file.fieldname !== 'cover_img') return res.cc('文章封面是必选参数！')
     res.send('ok')
 }
